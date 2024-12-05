d8b55fe0e73b29725deeb4a5d75639742f8515f7
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2024-12-05 16:03:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -286,7 +286,7 @@ const MovieDatails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
 			async function getMovieDetails() {
 				setIsLoading(true);
 				const res =
-					await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}
+					await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}
 			`);
 
 				const data = await res.json();
