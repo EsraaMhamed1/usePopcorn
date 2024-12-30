7adcfaad1cbdfbe7475314f99b13a451409cb0ee
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2024-12-30 21:18:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Main from './components/06challeng/Main';
+// import Main from './components/06challeng/Main';
 // import CoinTransform from './components/05challeng/CoinTransform';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		{/* <App /> */}
+		 <App /> 
 		{/* <CoinTransform /> */}
-		<Main />
+		// <Main />
 	</React.StrictMode>
 );
 
