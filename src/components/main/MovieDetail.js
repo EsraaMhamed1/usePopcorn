30b88822537a83912a471f0e547700ed7f265182
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2024-12-30 21:23:27 +0200

    Update MovieDetail.js

--- src/components/main/MovieDetail.js
+++ src/components/main/MovieDetail.js
@@ -38,7 +38,7 @@ export function MovieDetail({
 			setIsLoading(true);
 			async function fetchMovieDetails() {
 				const res = await fetch(
-					`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+					`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
 				);
 
 				const data = await res.json();
